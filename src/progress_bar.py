64fe8448b5fc64cd2d5f91e240d371b85e59fc1c
Lexi Mattick <lexi.mattick@gmail.com>
2026-03-17 14:30:57 -0700

    Toggle progress bar

--- src/progress_bar.py
+++ src/progress_bar.py
@@ -3,7 +3,7 @@ from typing import Union, Optional
 from aqt import QProgressBar, Qt, QWidget, QDockWidget, gui_hooks, mw, QLabel
 from anki.cards import Card
 from anki.collection import OpChanges
-from .util import get_cards_done_today, get_cards_due_today
+from .util import get_cards_done_today, get_cards_due_today, get_config, config_changed_hooks
 
 # Create the widget
 bar = QProgressBar()
@@ -26,6 +26,8 @@ widget.setWidget(bar)
 widget.setTitleBarWidget(QWidget())
 mw.addDockWidget(Qt.DockWidgetArea.TopDockWidgetArea, widget)
 
+enabled = False
+
 def update_progress_bar():
     deck = mw.col.decks.current() if mw.col else None
     if not deck:
@@ -43,17 +45,34 @@ def update_progress_bar():
 
 # Register GUI hooks
 def reviewer_did_show_question(card: Card):
+    if not enabled:
+        return
     update_progress_bar()
 
 gui_hooks.reviewer_did_show_question.append(reviewer_did_show_question)
 
 def operation_did_execute(changes: OpChanges, handler: Optional[object]):
+    if not enabled:
+        return
     if changes.study_queues:
         update_progress_bar()
 
 gui_hooks.operation_did_execute.append(operation_did_execute)
 
 def state_did_change(new_state: MainWindowState, old_state: MainWindowState):
+    if not enabled:
+        return
     update_progress_bar()
 
 gui_hooks.state_did_change.append(state_did_change)
+
+# Config hook
+def config_changed():
+    global enabled
+    enabled = get_config()["progress_bar"]["enabled"]
+    widget.setVisible(enabled)
+    if enabled:
+        update_progress_bar()
+
+config_changed()
+config_changed_hooks.append(config_changed)
